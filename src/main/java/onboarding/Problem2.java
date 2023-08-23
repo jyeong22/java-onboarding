8476476379c90bf69e6b599f51e01cd651f4d7da
Jiyoung Choi <12201817@inha.edu>
2023-08-23 13:48:08 +0900

    첫번재 문자와 마지막 문자인지 확인

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,33 @@
 package onboarding;
 
 public class Problem2 {
+    static boolean check = true;
     public static String solution(String cryptogram) {
         String answer = "answer";
+        while(check){
+            answer = getAnswerWithoutDuplicate(cryptogram);
+            if(answer == cryptogram || answer == ""){
+                check = false;
+            }
+            cryptogram = answer;
+        }
+
         return answer;
     }
+
+    private static String getAnswerWithoutDuplicate(String cryptogram) {
+        String temp_answer = "";
+        for (int i = 0; i < cryptogram.length(); ++i) {
+            if (i == 0) {
+
+            } else if (i == cryptogram.length() - 1) {
+
+            } else {
+
+            }
+        }
+        return temp_answer;
+    }
+
+
 }
